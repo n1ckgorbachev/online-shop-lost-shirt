b78e09238261ab75baf93e5ff2b9adb3be34d92b
Nikolay <n1kko.gorbachevv@gmail.com>
2021-01-08 02:04:55 +0300

    сделал верстку слайдера главной страницы

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,18 +1,30 @@
+
+//Переменные
 let openingButton = document.querySelector('.sidebar__hamburger');
 let closingButton = document.querySelector('.sidebar__close')
 let sidebar = document.querySelector('.sidebar');
 
-function openSidebar(){
-    sidebar.classList.add('sidebar--opened');
-}
 
-function hiddenMainlogo(){
+//Функции
+function hideLogo(){
     document.getElementById('main__logo').style.visibility = 'hidden';
 }
 
-openingButton.addEventListener('click', openSidebar, hiddenMainlogo);
-
-closingButton.addEventListener('click', function(){
-    sidebar.classList.remove('sidebar--opened');
+function showLogo(){
     document.getElementById('main__logo').style.visibility = 'visible';
-});
+}
+
+function openSidebar(){
+    sidebar.classList.add('sidebar--opened');
+    hideLogo();
+}
+
+function closeSidebar(){
+    sidebar.classList.remove('sidebar--opened');
+    showLogo();
+}
+
+
+//Основной скрипт
+openingButton.addEventListener('click', openSidebar);
+closingButton.addEventListener('click', closeSidebar);
